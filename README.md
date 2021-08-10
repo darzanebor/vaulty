47dc0b90369e23c543ad2d5a3977dd782d7a508e
denis_goncharov <info@muffs.ru>
2021-08-10 20:38:47 +0300

    Init

---
+++ README.md
@@ -0,0 +1,120 @@
+### Vaulty is a tool for obtaining credentials/secrets from Hashicorp Vault
+
+#### Environment Variables
+
+##### Global Env Vars
+#
+| Variable | Default | Example |
+| ------ | ------ | ------ |
+| VAULT_ADDR | | https://vault.example.com:8200 |
+| VAULT_ROLE | | aws_readonly |
+| VAULT_TTL | 3600 | 60 |
+| VAULT_AUTH | radis or approle or ldap | ldap |
+| VAULT_CRED | to stdout | ./credentials |
+| VAULT_MOUNT | | secret |
+| VAULT_SECRET | | gitlab |
+| VAULT_TLS_VER | | True |
+
+##### AppRole Auth
+#
+| Variable | Default |
+| ------ | ------ |
+| VAULT_USER | ROLE_ID |
+| VAULT_PASS | SECRET_ID |
+
+##### Radius Auth
+#
+| Variable | Default |
+| ------ | ------ |
+| VAULT_USER | |
+| VAULT_PASS | |
+
+##### Ldap Auth
+#
+| Variable | Default |
+| ------ | ------ |
+| VAULT_USER | |
+| VAULT_PASS | |
+
+##### How-To
+# 
+
+###### 0. Create AWS account which you would use with Vault.
+# 
+###### 1. Create Policy, arn:aws:iam::*:policy/VaultAdminPolicy (example)
+# 
+# 
+```
+{
+    "Version": "2012-10-17",
+    "Statement": [
+        {
+            "Effect": "Allow",
+            "Action": [
+                "ec2:*"
+            ],
+            "Resource": "*"
+        },
+        {
+            "Effect": "Allow",
+            "Action": [
+                "eks:*"
+            ],
+            "Resource": "*"
+        }
+    ]
+}
+```
+###### 2. Create Role, arn:aws:iam::*:role/VaultAdmin with attached policy arn:aws:iam::*:policy/VaultAdminPolicy and configure Trusted relationship with account from step 0.
+# 
+###### 3. Enable Vault Secret Engine with Access_Key and Secret_Key for account from step 0 and nesessary timeouts.
+# 
+###### 4. Create role default.admin in AWS Secret Engine with type AssumedRole and Role Arn: arn:aws:iam::*:role/VaultAdmin
+# 
+###### 5. Configure Authentication in Vault with Methods radius, app-role or ldap(tbd) and policy which would grant access to AWS Secrets Engine:
+# 
+# 
+```
+# Read and Update aws/sts/*
+path "aws/sts/*"
+{
+  capabilities = ["read","update"]
+}
+```
+#
+###### 6. If needed add nesessary mappings to aws-auth config map at EKS cluster
+# 
+# 
+```
+---
+apiVersion: v1 
+kind: ConfigMap 
+metadata: 
+  name: aws-auth 
+  namespace: kube-system 
+data: 
+  mapRoles: | 
+    - rolearn: arn:aws:iam::*:role/VaultAdmin
+      username: system:node:vaultadmin
+      groups: 
+        - system:masters
+```
+# 
+###### 7. Container usage example
+```
+docker run \
+    -e VAULT_AUTH="ldap" \
+    -e VAULT_ADDR="https://vault.example.local:8200" \
+    -e VAULT_USER="some_user" \
+    -e VAULT_PASS="some_password" \
+    -e VAULT_ROLE="default.admin" \
+    -e VAULT_TTL=60 \
+    alphaceti/aws-vaulty:0.1.3
+```
+# 
+###### 8. Gitlab Usage with Env Vars
+<div align="center" width="90%">
+<img src="./img/Screenshot_from_2021-08-10_18-03-03.png?raw=true" alt="Vault AWS Secret Engine">
+<img src="./img/Screenshot_from_2021-08-10_18-05-10.png?raw=true" alt="Vault KV2 Secret">
+</div>
+
---
+++ build/app/main.py
@@ -0,0 +1,137 @@
+#!/usr/bin/env python3
+""" Vaulty is a tool for obtaining temporary aws credentials from hashicorp vault """
+from datetime import datetime
+from os import environ as env
+from json import dumps
+from jinja2 import Template
+import hvac
+
+
+class aws_vaulty:
+    """Vaulty Class"""
+
+    def gen_data(self, vault_client):
+        """ generate aws credentials or get latest secret  from vault"""
+
+        if "aws" in self.vault_mount:
+            return vault_client.secrets.aws.generate_credentials(
+                name=self.vault_role,
+                ttl=self.vault_ttl,
+                endpoint="sts",
+                mount_point=self.vault_mount,
+            )
+        return vault_client.secrets.kv.v2.read_secret(
+            path=self.vault_secret, mount_point=self.vault_mount
+        )
+
+    def get_aws_creds_ldap(self):
+        """generate temporary aws credentials, auth method radius"""
+        vault_client = hvac.Client(url=self.vault_addr, verify=self.vault_verify)
+        vault_client.auth.ldap.login(
+            use_token=True,
+            username=self.vault_user,
+            password=self.vault_pass,
+        )
+        return self.gen_data(vault_client)
+
+    def get_aws_creds_radius(self):
+        """generate temporary aws credentials, auth method radius"""
+        vault_client = hvac.Client(url=self.vault_addr, verify=self.vault_verify)
+        vault_client.auth.radius.login(
+            use_token=True,
+            username=self.vault_user,
+            password=self.vault_pass,
+        )
+        return self.gen_data(vault_client)
+
+    def get_aws_creds_approle(self):
+        """generate temporary aws credentials, auth method approle"""
+        vault_client = hvac.Client(url=self.vault_addr, verify=self.vault_verify)
+        vault_client.auth.approle.login(
+            use_token=True,
+            role_id=self.vault_user,
+            secret_id=self.vault_pass,
+        )
+        return self.gen_data(vault_client)
+
+    def output_credentials(self):
+        """update local aws credentials file provided in path or to stdout"""
+        if not self.vault_cred:
+            print(dumps(self.vault_credentials["data"], sort_keys=True))
+        else:
+            with open("aws.credentials.jinja2", "r") as file_:
+                template = Template(file_.read())
+                aws_credentials_output = template.render(
+                    access_key=self.vault_credentials["data"]["access_key"],
+                    secret_key=self.vault_credentials["data"]["secret_key"],
+                    security_token=self.vault_credentials["data"]["security_token"],
+                )
+            with open(self.vault_cred, "w") as file_:
+                file_.write(aws_credentials_output)
+            print(
+                '{ "type": "credentials", "action":"aws credentials update", '
+                + '"message":"credentials update success", "severity": "medium", "date": "'
+                + datetime.now().strftime("%d/%m/%Y %H:%M:%S")
+                + '", "Lease Duration": "'
+                + str(self.vault_credentials["lease_duration"])
+                + '" }'
+            )
+
+    def get_secrets(self):
+        """generate credentials"""
+        if self.vault_method == "radius":
+            self.vault_credentials = self.get_aws_creds_radius()
+        elif self.vault_method == "approle":
+            self.vault_credentials = self.get_aws_creds_approle()
+        elif self.vault_method == "ldap":
+            self.vault_credentials = self.get_aws_creds_ldap()
+        if self.vault_credentials:
+            self.output_credentials()
+        else:
+            print(
+                '{"type": "credentials", "action":"aws credentials update",'
+                + '"message":"credentials update failed, no auth type configured",'
+                + '"severity": "medium", "date":"'
+                + datetime.now().strftime("%d/%m/%Y %H:%M:%S")
+                + '"}'
+            )
+
+    def check_empty(self, env_var):
+        """ env variables check """
+        if env_var == "" or not env_var:
+            return False
+        return True
+
+    def __init__(self):
+        self.vault_addr   = env.get("VAULT_ADDR", "")
+        self.vault_user   = env.get("VAULT_USER", "")
+        self.vault_pass   = env.get("VAULT_PASS", "")
+        self.vault_role   = env.get("VAULT_ROLE", "")
+        self.vault_ttl    = env.get("VAULT_TTL", 3600)
+        self.vault_method = env.get("VAULT_AUTH")
+        self.vault_cred   = (
+            env.get("VAULT_CRED")
+            if self.check_empty(env.get("VAULT_CRED"))
+            else False
+        )
+        self.vault_mount  = (
+            env.get("VAULT_MOUNT")
+            if self.check_empty(env.get("VAULT_MOUNT"))
+            else "aws"
+        )# aws or secret
+        self.vault_secret = (
+            env.get("VAULT_SECRET")
+            if self.check_empty(env.get("VAULT_SECRET"))
+            else "gitlab"
+         )# secret at mount for example secret/gitlab
+        self.vault_verify = (
+            env.get("VAULT_TLS_VER")
+            if self.check_empty(env.get("VAULT_TLS_VER"))
+            else False
+        )
+        self.vault_credentials = {}
+
+
+if __name__ == "__main__":
+    vaulty = aws_vaulty()
+    vaulty.get_secrets()
